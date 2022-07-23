e3879e3bfac7b289683f9ea168b237926e4bed9a
zaimo3869 <zaimo3869@gmail.com>
2022-07-23 19:12:58 +0200

    test 1

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// let bab = 2;
+// let pap = 6;
+// let r = bab+pap;
+
+// const message = (bab, pap) =>{
+//     alert("resultat:" + (bab + pap))
+// };
+
+
+// message(6, 2);
+// console.log(window)
